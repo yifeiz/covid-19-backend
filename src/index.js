62fe9b7ff7feffbcbff1b3a292f8cad34cae104c
Yifei Zhang <yifei7.zhang@gmail.com>
2020-03-16 13:26:10 -0400

    setup

---
+++ src/index.js
@@ -0,0 +1,48 @@
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 80;
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const basicAuth = require("express-basic-auth");
+const cookieParser = require("cookie-parser");
+const uuidv4 = require("uuid/v4");
+const MongoClient = require("mongodb").MongoClient;
+
+require("dotenv").config();
+
+const cloud = true;
+
+const url = cloud
+  ? `mongodb+srv://admin:${process.env.DBPASSWORD}@covid-19-09okh.mongodb.net/test?retryWrites=true&w=majority`
+  : "mongodb://127.0.0.1:27017";
+
+var db, collection, users;
+
+const dbName = "covid-19";
+
+MongoClient.connect(
+  url,
+  { useNewUrlParser: true, useUnifiedTopology: true },
+  (err, client) => {
+    if (err) return console.log(err);
+
+    db = client.db(dbName);
+
+    console.log(`Connected MongoDB: ${url}`);
+  }
+);
+
+app.use(cors({ origin: true, credentials: true }));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(bodyParser.raw());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.status(200).send("Setup");
+  console.log("hello");
+});
+
+app.listen(port, () => {
+  console.log("listening on port " + port + ".");
+});
