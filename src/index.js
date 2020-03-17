d71f4ab11ec3c831726e09b6e336e15d934e4ae6
William Wen <william.wen360@gmail.com>
2020-03-17 09:05:32 -0400

    You can now run npm start and have the app work properly

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const port = process.env.PORT || 80;
+const port = process.env.PORT || 3000;
 const cors = require("cors");
 const bodyParser = require("body-parser");
 const basicAuth = require("express-basic-auth");
