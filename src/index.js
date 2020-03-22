f033c150c2bca458893e848267e7e0e88bbaf607
Yifei Zhang <yifei7.zhang@gmail.com>
2020-03-22 16:34:34 -0400

    revert

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ require("dotenv").config();
 const express = require("express");
 const app = express();
 const cors = require("cors");
+const port = process.env.PORT || 80;
 const bodyParser = require("body-parser");
 const cookieParser = require("cookie-parser");
 const api = require("./api");
@@ -17,7 +18,60 @@ app.use(cookieParser("a2285a99-34f3-459d-9ea7-f5171eed3aba"));
 // Routers
 app.use("/api", api);
 
-const port = process.env.PORT || 80;
+// submit endpoint
+app.post("/submit", async (req, res) => {
+  const threatScore = flattenMatrix.getScoreFromAnswers(req.body);
+
+  let submission = {};
+  submission.timestamp = Date.now();
+  submission.ip_address = requestIp.getClientIp(req);
+
+  // Check if cookie value already exists; if not, generate a new one
+  if (req.signedCookies.userCookieValue) {
+    submission.cookie_id = req.signedCookies.userCookieValue;
+  } else {
+    submission.cookie_id = uuidv4();
+    const cookie_options = {
+      httpOnly: true,
+      signed: true,
+      domain: "flatten.ca"
+    };
+
+    res.cookie("userCookieValue", submission.cookie_id, cookie_options);
+  }
+
+  submission.at_risk = flattenMatrix.atRisk(req.body);
+  submission.probable = flattenMatrix.probable(req.body);
+  submission.form_responses = { ...req.body, timestamp: submission.timestamp };
+
+  // inserts/updates entity in dataStore
+  await googleData.insertForm(submission);
+
+  if (threatScore) {
+    const matrixResponse = flattenMatrix.getResponseFromScore(threatScore);
+
+    const responseJson = {
+      score: threatScore,
+      response: matrixResponse
+    };
+    res.status(200).json(responseJson);
+  } else {
+    throw new Error("Invalid Response");
+  }
+});
+
+// determines if a cookie already exists
+app.get("/read-cookie", (req, res) => {
+  let exists;
+  console.log(req.signedCookies.userCookieValue);
+  exists = req.signedCookies.userCookieValue ? true : false;
+  res.send({ exists });
+});
+
+//clears cookie
+app.delete("/clear-cookie", (req, res) => {
+  res.clearCookie("userCookieValue").send("success");
+});
 
 // submit endpoint
 
