2fe4e5a3abd6d458037b2dc33a9e74a9ef13e75a
Yifei Zhang <yifei7.zhang@gmail.com>
2020-03-22 21:58:44 -0400

    added history to init

--- src/dataStore.js
+++ src/dataStore.js
@@ -3,22 +3,23 @@ const { Datastore } = require("@google-cloud/datastore");
 const datastore = new Datastore();
 
 exports.insertForm = async submission => {
-  const key = datastore.key(
-      {"path": [process.env.DATASTORE_KIND, submission.cookie_id],
-        "namespace": process.env.DATASTORE_NAMESPACE});
+  const key = datastore.key({
+    path: [process.env.DATASTORE_KIND, submission.cookie_id],
+    namespace: process.env.DATASTORE_NAMESPACE
+  });
 
   try {
     // Try to insert an object with cookie_id as key. If already submitted, fails
     const entity = {
       key,
-      data: { ...submission, history: [] }
+      data: { ...submission, history: [submission.form_responses] }
     };
     await datastore.insert(entity);
   } catch (e) {
     // If it already exists, update with new history
     let [data] = await datastore.get(key);
 
-    data.history.push(data.form_responses);
+    data.history.push(submission.form_responses);
     data.form_responses = submission.form_responses;
     data.timestamp = submission.timestamp;
     data.at_risk = submission.at_risk;
