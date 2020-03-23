5dab29947a2f767300af19c039d85be55b4037f9
Owen <obrake3@gmail.com>
2020-03-22 21:50:54 -0400

    Added Secure tag

--- src/index.js
+++ src/index.js
@@ -36,6 +36,7 @@ app.post("/submit", async (req, res) => {
       httpOnly: true,
       signed: true,
       domain: "flatten.ca",
+      secure: true,
       maxAge: 1000 * 60 * 60 * 24 * 365 * 2, //maxAge is ms thus this is 2 years
     };
 
